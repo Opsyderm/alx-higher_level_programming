082743551db4e81b6ba1dfd26bcf1c158ed17f50
opsyderm <opeyemiolaniyan628@gmail.com>
2023-09-05 07:44:22 -0700

    pystyle

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -5,4 +5,3 @@
 def add(a, b):
     """Returns the addition of a and b."""
     return (a + b)
-
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -6,4 +6,3 @@ def print_last_digit(number):
     """Prints the last digit of a number and returns it."""
     print(abs(number) % 10, end="")
     return (abs(number) % 10)
-
