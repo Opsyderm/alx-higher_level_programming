0aa313664e6e15e5230f17c54d3b4c0351258a88
opsyderm <opeyemiolaniyan628@gmail.com>
2023-09-07 05:27:20 -0700

    import modules

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My sum function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """My difference function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """the multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """the division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        return value. a / b
+    """
+    return int(a / b)
