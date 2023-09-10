f383ab09a04c51acb7d86f4fe63754700f68c536
opsyderm <opeyemiolaniyan628@gmail.com>
2023-09-10 13:32:33 -0700

    data structure

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -12,4 +12,3 @@ def divisible_by_2(my_list=[]):
             multiples.append(False)
 
     return (multiples)
-
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -7,4 +7,3 @@ def multiple_returns(sentence):
     if sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
-
